595a2f537fb9a0ecebd18e5d1d2300c7c7fc60a8
Jamil Bou Kheir <jamilboukheir@me.com>
2013-09-27 13:26:34 -0700

    coverage support for mocha

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 var expect = require('chai').expect
   , assert = require('chai').assert
-  , User = require('../models/user').model;
+  , User = require('../lib/models/user').model;
 
 assert(User !== undefined, 'User should be present');
 
