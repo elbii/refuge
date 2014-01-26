26777c6e2a6b34b623749d68a78ad8650af924c5
Jamil Bou Kheir <jamil@elbii.com>
2014-01-26 15:58:31 -0800

    bowercopy to the rescue

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    bowercopy: {
+      options: {
+        clean: true
+      },
+      libs: {
+        options: {
+          destPrefix: 'lib/public/js/vendor'
+        },
+        files: {
+          'backbone.js': 'backbone/backbone.js',
+          'jQuery.serializeObject.js': 'jQuery.serializeObject/jQuery.serializeObject.js',
+          'jquery.js': 'jquery/jquery.js',
+          'requirejs-text.js': 'requirejs-text/text.js',
+          'require.js': 'requirejs/require.js',
+          'sjcl.js': 'sjcl/sjcl.js',
+          'underscore.string.js': 'underscore.string/lib/underscore.string.js',
+          'underscore.js': 'underscore/underscore.js'
+        }
+      },
+      dev: {
+        options: {
+          destPrefix: 'test/browser',
+        },
+        files: {
+          'mocha.js': 'mocha/mocha.js',
+          'mocha.css': 'mocha/mocha.css',
+          'chai.js': 'chai/chai.js'
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('default', 'Hello Grunt', function () {
+    console.log('HELLO GRUNT');
+  });
+
+  grunt.loadNpmTasks('grunt-bowercopy');
+};
